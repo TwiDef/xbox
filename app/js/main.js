2f00e140ea3ca4e2e8b736b533785f75ae20301b
Def <deferenseo@gmail.com>
2023-02-22 18:06:38 +0300

    finished top section + added slick-carousel

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-console.log('Lorem');
+$(function() {
+    $('.slider').slick({
+        arrows: true,
+        dots: false,
+        fade: true,
+        prevArrow: '<button type="button" class="slider__arrow slider__arrowleft"><img src="images/back.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slider__arrow slider__arrowright"><img src="images/next.svg" alt="right"></button>',
+    });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ function images() {
 function scripts() {
     return src([
             'node_modules/jquery/dist/jquery.js',
-
+            'node_modules/slick-carousel/slick/slick.js',
             'app/js/main.js'
         ])
         .pipe(concat('main.min.js'))
@@ -52,7 +52,10 @@ function scripts() {
 }
 
 function styles() {
-    return src('app/scss/style.scss')
+    return src(['app/scss/style.scss',
+            'node_modules/slick-carousel/slick/slick.scss',
+            'node_modules/slick-carousel/slick/slick-theme.scss',
+        ])
         .pipe(scss({ outputStyle: 'compressed' })) // 'expanded'
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
