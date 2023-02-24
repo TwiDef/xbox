f71c6ab4d5f178f404d5139fc96d5ccd97ee536f
Def <deferenseo@gmail.com>
2023-02-24 14:36:21 +0300

    finished top block

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,5 @@ $(function() {
         prevArrow: '<button type="button" class="slider__arrow slider__arrowleft"><img src="images/back.svg" alt="left"></button>',
         nextArrow: '<button type="button" class="slider__arrow slider__arrowright"><img src="images/next.svg" alt="right"></button>',
     });
+    new ModalVideo('.js-modal-btn', { url: 'https://youtu.be/_q51LZ2HpbE' });
 });
