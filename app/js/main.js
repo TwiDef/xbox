c46fbd831dabc0a30653cc0156d50ab29461a9ec
Def <deferenseo@gmail.com>
2023-02-27 14:24:42 +0300

    done exclusive block

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,14 @@ $(function() {
         prevArrow: '<button type="button" class="slider__arrow slider__arrowleft"><img src="images/back.svg" alt="left"></button>',
         nextArrow: '<button type="button" class="slider__arrow slider__arrowright"><img src="images/next.svg" alt="right"></button>',
     });
+
+    $('.slider-cards').slick({
+        slidesToShow: 4,
+        variableWidth: true,
+        infinite: true,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
 });
